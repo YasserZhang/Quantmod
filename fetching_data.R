1fa94841a3822c1dd62b965b69e80132a31fa77f
YasserZhang <yaseru2003@gmail.com>
2014-08-29 01:42:30 -0400

    homework to be finished

--- fetching_data.R
+++ fetching_data.R
@@ -1,3 +1,4 @@
+library(quantmod)
 # set a new environment for upcoming data
 new.environment <- new.env()
 # fetch a shanghai stock exchange stock china petrol from google
@@ -51,7 +52,113 @@ getSymbols("CHL")
 setSymbolLookup(SANY.HEAVY = list(name="600030.ss", src = "yahoo"))
 getSymbols("SANY.HEAVY")
 
+"""
+quantmod的ETL函数下载Apple,Microsoft,Oracle,Google四家公司全量股票行情数据
 
+1)求出Apple公司在2013.1-2013.10的股票总成交量使用
+"""
+library(quantmod)
+# set a new environment for upcoming data
+new.environment <- new.env()
+getSymbols("AAPL", env=new.environment, scr="yahoo", from = "2013-01-01", to = "2013-10-01")
+ls(new.environment)
+Apple <- get("AAPL", envir = new.environment)
+summary(Apple)
+dim(Apple)
+# 重命名列名
+names(Apple)  <- c("Open", "High", "Low", "Close", "Volume", "Adjusted")
+head(Apple$Volume)
+# 计算交易量总和
+sum(Apple$Volume)
+20,663,792,100 股
+
+"""
+2)找出这些股票暴涨暴跌的时间点（例如开盘价或收盘价比前一天涨跌幅度超过2%），
+通过搜索引擎寻找是什么原因导致这些暴涨暴跌，观察（或用程序分析）数据，看就暴涨暴跌事件是否有可以利用的买卖规律
+"""
+首先看AAPL
+getSymbols("AAPL", src="yahoo")
+# 重新命名数据框列名
+name <- c("Open","High","Low","Close","Volume","Adjusted")
+names(AAPL) <- name
+# 每日股票收盘价的变化率，即计算后一天收盘价与前一天收盘价的价差并除以前天收盘价。
+change_rate_A <- (Next(AAPL$Close, 1)[-length(AAPL$Close)] - AAPL$Close[-length(AAPL$Close)])/AAPL$Close[-length(AAPL$Close)]
+# 列出所有变化率大于2%或小于-2%的日期及具体数量。
+big_change_A <- change_rate_A[which(change_rate_A > 0.02 | change_rate_A < -0.02)]
+big_change_A
+# 求股价有大变动的日期的总数
+length(big_change_A)
+[1] 501
+# 列出其中的最大值
+big_change_A[which(big_change_A == max(big_change_A))]
+2008-10-10
+0.1390496
+# 列出其中的最小值
+big_change_A[which(big_change_A == min(big_change_A))]
+2014-06-06
+-0.8548569
+"""
+由于股价有大变动的日期太多，有501天，在此无法把每次的原因一一罗列，所以我只挑最大值和最小值重点解释。
+最大值发生在2008年10月10日。10月3日，关于乔布斯的健康传言进一步打压苹果股票，到10月9日以到一个新低点，但由于10月14日苹果即将推出新的苹果笔记本产品以及10月21日公司即将发布新一季度财报等原因，股价至此开始强烈触底反弹。
+最小值发生在2014年6月6日，原因是苹果分拆股票。
+"""
+getSymbols("MSFT", src="yahoo")
+# 重新命名数据框列名
+name <- c("Open","High","Low","Close","Volume","Adjusted")
+names(MSFT) <- name
+# 每日股票收盘价的变化率，即计算后一天收盘价与前一天收盘价的价差并除以前天收盘价。
+change_rate_M <- (Next(MSFT$Close, 1)[-length(MSFT$Close)] - MSFT$Close[-length(MSFT$Close)])/MSFT$Close[-length(MSFT$Close)]
+# a better way to calculate change rate using Delt()
+change_rate_M <- Delt(Cl(MSFT),type= ("arithmetic"))
+# 列出所有变化率大于2%或小于-2%的日期及具体数量。
+big_change_M <- change_rate_M[which(change_rate_M > 0.02 | change_rate_M < -0.02)]
+big_change_M
+# 求股价有大变动的日期的总数
+length(big_change_M)
+# 列出其中的最大值
+big_change_M[which(big_change_M == max(big_change_M))]
+# 列出其中的最小值
+big_change_M[which(big_change_M == min(big_change_M))]
+
+getSymbols("AAPL", src="yahoo")
+getSymbols("MSFT", src="yahoo")
+getSymbols("ORCL", src="yahoo")
+getSymbols("GOOG", src="yahoo")
+tickers <- c("AAPL","MSFT","ORCL","GOOG")
+getSymbols(tickers,env=new.environment, src = "yahoo")
+chartSeries(AAPL, subset = "last 12 months")
+chartSeries(MSFT, subset = "last 12 months")
+chartSeries(ORCL, subset = "last 12 months")
+chartSeries(GOOG, subset = "last 12 months")
+head(get("AAPL"))
+ls()
+# a experiment, testing how to calculate the change rate from the Close price to the Open price
+apple <- last(AAPL, 50)
+head(apple)
+(Next(apple,1)[-50] - apple$AAPL.Close[-50])/apple$AAPL.Close[-50]
+# the experiment is succesful!
+apple <- last(AAPL$AAPL.Close, 50)
+head(apple)
+rate <- (Next(apple,1)[-50] - apple$AAPL.Close[-50])/apple$AAPL.Close[-50]
+rate[which(rate > 0.02 | rate < -0.02)]
+(95.97-94.03)/94.03
+names(AAPL)
+name <- c("Open","High","Low","Close","Volume","Adjusted")
+names(AAPL) <- name
+names(MSFT) <- name
+names(ORCL) <- name
+names(GOOG) <- name
+nrow(AAPL)
+change_rate_A <- (Next(AAPL$Close, 1)[-length(AAPL$Close)] - AAPL$Close[-length(AAPL$Close)])/AAPL$Close[-length(AAPL$Close)]
+big_change_A <- change_rate_A[which(change_rate_A > 0.02 | change_rate_A < -0.02)]
+min(big_change_A)
+summary(big_change_A)
+chartSeries(AAPL)
+
+"""
+3)截取一段时间内这四家公司股价数据（注意分红派息除权对股价的影响），
+用R中的相关性分析判断股价之间的相关性，或者用R基础课程第八周所讲的MIC指标对其进行分析
+"""
 
 
 # 从多种信息源里获得信息
